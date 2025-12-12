fccd374a22ae9037f90105770ba9920e94e784df
KekeEmmanuel <KekeEmmanuel@users.noreply.github.com>
2025-12-12 20:41:58 +0300

    Update pull-updates to include Home.vue file

--- pull-updates.php
+++ pull-updates.php
@@ -111,6 +111,7 @@ $filesToCopy = [
     'app/Http/Controllers/Admin/ContactMessageController.php',
     'app/Http/Controllers/Admin/ContactChannelController.php',
     'app/Http/Controllers/Admin/ContactQuickFactController.php',
+    'resources/marketing/pages/Home.vue',
 ];
 
 foreach ($filesToCopy as $file) {
